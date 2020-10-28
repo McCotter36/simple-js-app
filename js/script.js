6325c8020c85bb9b80be04580ab1b045944971ac
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2020-10-28 12:53:29 -0600

    update script file wih array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-alert('Hello World');
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Ninetales',
+  height: 1.1,
+  types: ['fire']}
+  {name: 'Gligar',
+  height: 1.1,
+  types: ['ground','flying']}
+  {name: 'Girafarig',
+  height: 1.5,
+  types: ['psychic', 'normal']}
+];
