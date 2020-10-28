c9a554666685cbb9baa9d68d3c4abcd056ef881f
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2020-10-28 12:59:02 -0600

    update array with new object

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 let pokemonList = [
   {name: 'Ninetales',
   height: 1.1,
-  types: ['fire']}
+  types: ['fire'],
+  abilities: ['Flash-fire', 'Drought']}
   {name: 'Gligar',
   height: 1.1,
-  types: ['ground','flying']}
+  types: ['ground','flying'].
+  abilities: 'Sand-veil', 'Immunity', 'Hyper-cutter'}
   {name: 'Girafarig',
   height: 1.5,
-  types: ['psychic', 'normal']}
+  types: ['psychic', 'normal'],
+  abilities: ['Inner-focus', 'Early-bird', 'Sap-sipper'] }
+  {name: 'Fearow',
+  height: 1.2,
+  types: ['flying','normal'],
+  abilities: ['Keen-eye','sniper']}
 ];
