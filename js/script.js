7be4ab7fba75f6659f2416d9849d762415266884
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2020-10-30 10:25:15 -0600

    added IIFE for Repository
    -moved list inside IIFE
    -rewrote function to access Repository
    -added variabe pokemonAttribute to shorten code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,52 @@
-let pokemonList = [
-  {
-    name: 'Ninetales',
-    height: 1.1,
-    types: ['fire'],
-    abilities: ['Flash-fire', 'Drought']
-  },
-  {
-    name: 'Gligar',
-    height: 1.1,
-    types: ['ground','flying'],
-    abilities: ['Sand-veil', 'Immunity', 'Hyper-cutter']
-  },
-  {
-    name: 'Girafarig',
-    height: 1.5,
-    types: ['psychic', 'normal'],
-    abilities: ['Inner-focus', 'Early-bird', 'Sap-sipper']
-  },
-  {
-    name: 'Fearow',
-    height: 1.2,
-    types: ['flying','normal'],
-    abilities: ['Keen-eye','sniper']},
-];
-
-function myLoopFunction(pokemon) {
-    if(pokemon.height > 1.4){
-      document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm) -Oh my, that\'s huge! </p>');
-    } else {
-      document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm) </p>');
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Ninetales',
+      height: 1.1,
+      types: ['fire'],
+      abilities: ['Flash-fire', 'Drought']
+    },
+    {
+      name: 'Gligar',
+      height: 1.1,
+      types: ['ground','flying'],
+      abilities: ['Sand-veil', 'Immunity', 'Hyper-cutter']
+    },
+    {
+      name: 'Girafarig',
+      height: 1.5,
+      types: ['psychic', 'normal'],
+      abilities: ['Inner-focus', 'Early-bird', 'Sap-sipper']
+    },
+    {
+      name: 'Fearow',
+      height: 1.2,
+      types: ['flying','normal'],
+      abilities: ['Keen-eye','sniper']
     }
-  }
+  ];
 
-pokemonList.forEach(myLoopFunction);
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  };
+
+  function getAll() {
+    return pokemonList;
+  };
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  let pokemonAttributes = pokemon.name + ' (height: ' + pokemon.height
+    if(pokemon.height > 1.4){
+      document.write('<p>' + pokemonAttributes + 'm) -Oh my, that\'s huge! </p>');
+    } else {
+      document.write('<p>' + pokemonAttributes + 'm) </p>');
+    }
+  }) ();
